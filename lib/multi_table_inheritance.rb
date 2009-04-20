2f0598cca603aa27dbdd1434b901eb310af6125c
Justin Balthrop <justin@geni.com>
2009-04-20 11:38:36 -0700

    make multi-table inheritance behave more like single-table inheritance

--- lib/multi_table_inheritance.rb
+++ lib/multi_table_inheritance.rb
@@ -1,7 +1,59 @@
 module MultiTableInheritance
   module ActiveRecord
-    def multi_table_inheritance
+    def multi_table_inheritance(opts = {})
       set_table_name(name.underscore.pluralize)
+      base_class.send(:extend, MultiTableInheritance::ActiveRecord::ClassMethods)
+      base_class.send(:include, MultiTableInheritance::ActiveRecord::InstanceMethods)
+      base_class.multi_table_inheritance_opts(self, opts)
+    end
+
+    module ClassMethods
+      def multi_table_inheritance_opts(sub_class, opt)
+        @multi_table_inheritance_opts ||= {}
+        if opt.kind_of?(Hash)
+          @multi_table_inheritance_opts[sub_class] = opt
+        else
+          @multi_table_inheritance_opts[sub_class][opt]
+        end
+      end
+      
+      def default_select(qualified)
+        if qualified
+          "#{quoted_table_name}.*, #{quoted_table_name}.tableoid"
+        else
+          '*, tableoid'
+        end
+      end
+
+      def construct_finder_sql(options)
+        sql = super(options)
+        sql.sub(' *', ' *, tableoid').sub(/(\w+\.)\*/, '\1.*, \1.tableoid')
+      end
+      
+      def instantiate(record)
+        record_class = oid_to_class(record['tableoid'])
+        if record_class != self
+          if base_class.multi_table_inheritance_opts(record_class, :add_fields)
+            record_class.find(record['id'])
+          else
+            record_class.instantiate(record)
+          end
+        else
+          super
+        end
+      end
+
+      def oid_to_class(oid)
+        return self unless oid
+        @oid_to_class ||={}
+        @oid_to_class[oid] ||= connection.select_value("select relname from pg_class where oid = #{oid}").classify.constantize
+      end
+    end
+
+    module InstanceMethods
+      def type
+        self.class.oid_to_class(attributes['tableoid']).name
+      end
     end
   end
 
